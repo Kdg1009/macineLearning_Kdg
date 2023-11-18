acf84fe751a4817874c544fbde084038c3f4cf21
Kdg1009 <ehrms1009@hanmail.net>
2023-11-18 06:09:42 +0000

    DELETE: macineLearning_Kdg-master/ *.py *.ipynb files

--- imgSizeCheck.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import os
-
-path='/project/python_project/carDetect/project/test_set/test_3.labled/1.[[(0,0), (0,0)]].jpg'
-img=cv2.imread(path)
-print(img.shape)
-print(img[2][428][0])
